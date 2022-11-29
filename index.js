5f84adf10b79f4cd62dfe504b4d106945351c8fc
k-mlcvc <kristinamilicevic398@gmail.com>
2022-11-29 19:19:19 +0100

    done

---
+++ index.js
@@ -0,0 +1,40 @@
+// 1. zadatak
+var myAge = 24;
+console.log(myAge * 7);
+
+// 2. zadatak
+var myFirstName = "Kristina";
+var myLastName = "Miličević";
+var myFullName = `${myFirstName} ${myLastName}`;
+console.log(myFullName);
+
+// 3. zadatak
+var nineteen = 19;
+var five = 5;
+console.log(nineteen % five);
+
+// 4. zadatak
+var brainlabStudentCount = 22;
+console.log(brainlabStudentCount);
+var brainlabStudentCount = brainlabStudentCount + 2;
+console.log(brainlabStudentCount);
+var brainlabStudentCount = brainlabStudentCount - 3;
+console.log(brainlabStudentCount);
+var brainlabStudentCount = brainlabStudentCount + 5;
+console.log(brainlabStudentCount);
+
+// 5. zadatak
+// 33
+// 17
+// 54
+// My points: 49
+// 4
+// 1114
+
+// 6. zadatak
+console.log(myFullName);
+console.log(myFullName.toUpperCase());
+console.log(myFullName.toLowerCase());
+
+// 7. zadatak
+console.log("KristinaMiličević".length);
